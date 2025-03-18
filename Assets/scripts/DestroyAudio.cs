113fb81e8ebfd702381b17c5b0ade03c0c7e15a7
diegor90 <ramirezdiego284@gmail.com>
2025-03-18 10:52:36 -0700

    ui
    ui for main menu to game level

---
+++ Assets/scripts/DestroyAudio.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class DestroyAudio : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
+    {
+        Destroy(gameObject, 3f);
+    }
+
+     
+   
+}
---
+++ Assets/scripts/PlayAudio.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class PlayAudio : MonoBehaviour
+{
+    public GameObject audioObj;
+
+    public void DropAudio()
+    {
+        Instantiate(audioObj, transform.position, transform.rotation);
+    }
+    
+      
+  
+}
